492f003eab65c263ce20dd96d42ae20f0bba888e
caiscoding <2022133838@qq.com>
2022-06-21 03:09:02 +0800

    add lab05 code

---
+++ labs/lab05/lab05/parsons_probs/replace_elements.py
@@ -0,0 +1,22 @@
+def replace_elements(source_list, dest_list):
+    """
+    Complete the function replace_elements, a function which takes in source_list
+    and dest_list and mutates the elements of dest_list to be the elements at the
+    corresponding index in source_list.
+    
+    dest_list always has a length greater than or equal to the length of
+    source_list.
+
+    >>> s1 = [1, 2, 3]
+    >>> s2 = [5, 4]
+    >>> replace_elements(s2, s1)
+    >>> s1
+    [5, 4, 3]
+    >>> s3 = [0, 0, 0, 0, 0]
+    >>> replace_elements(s1, s3)
+    >>> s3
+    [5, 4, 3, 0, 0]
+    """
+    "*** YOUR CODE HERE ***"
+    for i in range(len(source_list)):
+        dest_list[i] = source_list[i]
