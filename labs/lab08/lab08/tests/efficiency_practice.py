5f7d7fca07f30aa9565672517fa432ebdf72368e
caiscoding <2022133838@qq.com>
2022-06-27 19:30:06 +0800

    add lab08 code

---
+++ labs/lab08/lab08/tests/efficiency_practice.py
@@ -0,0 +1,57 @@
+test = {
+  'name': 'efficiency_practice',
+  'points': 0,
+  'suites': [
+    {
+      'cases': [
+        {
+          'answer': 'Exponential',
+          'choices': [
+            'Constant',
+            'Logarithmic',
+            'Linear',
+            'Quadratic',
+            'Exponential',
+            'None of these'
+          ],
+          'hidden': False,
+          'locked': False,
+          'multiline': False,
+          'question': 'The count_partitions function runs in ____ time in the length of its input.'
+        },
+        {
+          'answer': 'Linear',
+          'choices': [
+            'Constant',
+            'Logarithmic',
+            'Linear',
+            'Quadratic',
+            'Exponential',
+            'None of these'
+          ],
+          'hidden': False,
+          'locked': False,
+          'multiline': False,
+          'question': 'The is_palindrome function runs in ____ time in the length of its input.'
+        },
+        {
+          'answer': 'Logarithmic',
+          'choices': [
+            'Constant',
+            'Logarithmic',
+            'Linear',
+            'Quadratic',
+            'Exponential',
+            'None of these'
+          ],
+          'hidden': False,
+          'locked': False,
+          'multiline': False,
+          'question': 'The binary_search function runs in ____ time in the length of its input.'
+        }
+      ],
+      'scored': False,
+      'type': 'concept'
+    }
+  ]
+}
